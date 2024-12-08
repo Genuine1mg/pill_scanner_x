85a48e20f9871d327a32a01338e5abb70d88b2c5
Yogesh <yogesh.jabgir@1mg.com>
2024-12-08 15:49:50 +0530

    all allowed host

--- pillscanner_backend/pillscanner_backend/settings.py
+++ pillscanner_backend/pillscanner_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*v4yoqlnshvf655&98cg_*er$6bc8fqhxtn+)ualnz5sf6utnb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pillscanner.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
